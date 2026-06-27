6e51b0e3bda1390527581234dd6a19e280916bb0
WilNerZero19 <eduherst19@gmail.com>
2026-06-27 14:18:45 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="arr-photo-edit-v1-2026-unique",
     version="0.1.0",
     author="arrafa.dev",
-    author_arrafaaddress@gmail.com",
+    author_email="arrafaaddress@gmail.com",
     description="Deskripsi projek saya",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
