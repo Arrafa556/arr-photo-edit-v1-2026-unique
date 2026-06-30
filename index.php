49a694f36648d12d2a8b1747662f64d6ac29b8ca
ArrafaMa19 <arrafaaddress@gmail.com>
2026-06-30 17:45:00 +0700

    upload php file
    koneksi php and index php simple program

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+  $name =  "Arrafa";
+  $umur =  "16 Tahun";
+?>
+
+<!DOCTYPE html >
+<html>
+<head>
+    <title>Belajar Php<title>
+</head>
+<body>
+    <h1>Halo, <?php echo $name; ?></h1>
+    <h2>halo, <?php echo $umur; ?></h2>
+</body>
+        </html>
+    <Data><a href="mailto:arrafaaddress@gmail.com"></a></Data>
+    </head>
