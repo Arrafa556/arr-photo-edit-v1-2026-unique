b660b28bae6b2b175d6a61f8cb139f5b27dd5b02
ArrafaMa19 <arrafaaddress@gmail.com>
2026-07-03 21:44:52 +0700

    app python Program Data

---
+++ app.py
@@ -0,0 +1,9 @@
+print("===DATA DIRI===\n")
+nama = input("Masukan Nama Anda:..")
+print(f"Nama anda:.. {nama} \n")
+id = input("Masukan NIM Anda:..")
+print(f"NIM anda:.. {id} \n")
+if nama == "ARR" and id == "123":
+    print("login Berhasil")
+else:
+     print("login Gagal")
